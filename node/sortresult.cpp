3a4149d66a300f9e762a4379e403faf31e7d03c7
obiwankennedy <renaud@rolisteam.org>
2014-01-07 18:57:45 +0100

    Update sortresult.cpp
    add diceresult

--- node/sortresult.cpp
+++ node/sortresult.cpp
@@ -1,10 +1,14 @@
 #include "sortresult.h"
 
 #include <QDebug>
+#include "die.h"
 
 SortResultNode::SortResultNode()
+    : m_diceResult(new DiceResult)
 {
     m_ascending = true;
+    m_result = m_diceResult;
+
 }
 void SortResultNode::run(ExecutionNode* node)
 {
@@ -12,26 +16,39 @@ void SortResultNode::run(ExecutionNode* node)
     {
         return;
     }
-    QList<Die> diceList=node->getResult()->getResultList();
-    QList<Die> diceList2=m_result.getResultList();
+    DiceResult* previousDiceResult = static_cast<DiceResult*>(node->getResult());
+    if(NULL!=previousDiceResult)
+    {
+        QList<Die> diceList=previousDiceResult->getResultList();
+        QList<Die> diceList2=m_diceResult->getResultList();
 
-    diceList2 = diceList;
-    if(!m_ascending)
-    {
-        qSort(diceList2.begin(), diceList2.end(), qGreater<int>());
-    }
-    else
-    {
-        qSort(diceList2.begin(), diceList2.end(), qLess<int>());
-    }
-    m_result.setResultList(diceList2);
-    if(NULL!=m_nextNode)
-    {
-        m_nextNode->run(this);
+        diceList2 = diceList;
+        if(!m_ascending)
+        {
+            qSort(diceList2.begin(), diceList2.end(), qGreater<Die>());
+        }
+        else
+        {
+            qSort(diceList2.begin(), diceList2.end(), qLess<Die>());
+        }
+        m_diceResult->setResultList(diceList2);
+        if(NULL!=m_nextNode)
+        {
+            m_nextNode->run(this);
+        }
     }
 
 }
 void SortResultNode::setSortAscending(bool asc)
 {
     m_ascending = asc;
+}
+
+
+bool operator<(Die const &a, Die const& b)
+{
+    if(a.getValue()<b.getValue())
+        return true;
+    else
+        return false;
 }
