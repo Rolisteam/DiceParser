a0729413733160e3915b630485144c22cd0c3566
obiwankennedy <renaud@rolisteam.org>
2014-01-07 18:52:10 +0100

    Update keepdiceexecnode.h
    Better API

--- node/keepdiceexecnode.h
+++ node/keepdiceexecnode.h
@@ -2,6 +2,7 @@
 #define KEEPDICEEXECNODE_H
 
 #include "executionnode.h"
+#include "diceresult.h"
 
 class KeepDiceExecNode : public ExecutionNode
 {
@@ -12,6 +13,7 @@ public:
     virtual void setDiceKeepNumber(quint64 );
 private:
     quint64 m_numberOfDice;
+    DiceResult* m_diceResult;
 };
 
 #endif // KEEPDICEEXECNODE_H
