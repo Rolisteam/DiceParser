7e638d99947fc242e9f6701710296f8995f139cb
obiwankennedy <renaud@rolisteam.org>
2014-01-07 18:48:50 +0100

    Update countexecutenode.h
    better api

--- node/countexecutenode.h
+++ node/countexecutenode.h
@@ -4,6 +4,7 @@
 #include "executionnode.h"
 
 #include "validator.h"
+#include "scalarresult.h"
 
 class CountExecuteNode : public ExecutionNode
 {
@@ -16,7 +17,7 @@ public:
 
 private:
     Validator* m_validator;
-
+    ScalarResult* m_scalarResult;
 };
 
 #endif // COUNTEXECUTENODE_H
