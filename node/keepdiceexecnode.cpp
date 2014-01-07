62d37d86caa5704fccc0d9c3a41d6e0d22e9603d
obiwankennedy <renaud@rolisteam.org>
2014-01-07 18:52:28 +0100

    Update keepdiceexecnode.cpp
    better API

--- node/keepdiceexecnode.cpp
+++ node/keepdiceexecnode.cpp
@@ -1,7 +1,13 @@
+#include <QList>
+
+
 #include "keepdiceexecnode.h"
 
+
 KeepDiceExecNode::KeepDiceExecNode()
+    : m_diceResult(new DiceResult())
 {
+    m_result = m_diceResult;
 }
 
 void KeepDiceExecNode::run(ExecutionNode* previous)
@@ -10,15 +16,19 @@ void KeepDiceExecNode::run(ExecutionNode* previous)
     {
         return;
     }
-    QList<qint64> diceList=previous->getResult()->getResultList();
-    QList<qint64> diceList2=m_result.getResultList();
-
-
-    diceList2 = diceList.mid(0,m_numberOfDice);
-    m_result.setResultList(diceList2);
-    if(NULL!=m_nextNode)
+    DiceResult* previousDiceResult = static_cast<DiceResult*>(previous->getResult());
+    if(NULL!=previousDiceResult)
     {
-        m_nextNode->run(this);
+        QList<Die> diceList=previousDiceResult->getResultList();
+        QList<Die> diceList2=m_diceResult->getResultList();
+
+
+        diceList2 = diceList.mid(0,m_numberOfDice);
+        m_diceResult->setResultList(diceList2);
+        if(NULL!=m_nextNode)
+        {
+            m_nextNode->run(this);
+        }
     }
 }
 void KeepDiceExecNode::setDiceKeepNumber(quint64 n)
