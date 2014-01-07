bcf1b0c9c285230b28126e270deac12a88d12ebd
obiwankennedy <renaud@rolisteam.org>
2014-01-07 18:43:20 +0100

    Update dicerollernode.cpp
    insert roll value instead of value

--- node/dicerollernode.cpp
+++ node/dicerollernode.cpp
@@ -6,21 +6,23 @@
 #include <QDebug>
 
 DiceRollerNode::DiceRollerNode(quint64 faces)
-    : m_faces(faces)
+    : m_faces(faces),m_myDiceResult(new DiceResult())
 {
     uint seed = quintptr(this) + QDateTime::currentDateTime().toMSecsSinceEpoch();
     qsrand(seed);
+    m_result=m_myDiceResult;
+
 }
 void DiceRollerNode::run(ExecutionNode* previous)
 {
     if(NULL!=previous)
     {
-        m_diceCount = previous->getResult()->getSum();
+        m_diceCount = previous->getResult()->getScalar();
         for(quint64 i=0; i < m_diceCount ; ++i)
         {
             Die die;
-            die.setValue(rollDice());
-            m_result.insertResult(die);
+            die.insertRollValue(rollDice());
+            m_myDiceResult->insertResult(die);
         }
         if(NULL!=m_nextNode)
         {
