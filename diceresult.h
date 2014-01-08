d52b42dff5a2944fa22f6da5241086ae0128b17a
obiwankennedy <renaud@rolisteam.org>
2014-01-08 18:18:15 +0100

    Update diceresult.h
    die*

--- diceresult.h
+++ diceresult.h
@@ -11,15 +11,15 @@ public:
     DiceResult();
 
     qint64 getSum();
-    QList<Die>& getResultList();
-    void insertResult(Die);
+    QList<Die*>& getResultList();
+    void insertResult(Die*);
 
-    void setResultList(QList<Die> list);
+    void setResultList(QList<Die*> list);
 
     bool isScalar() const;
     virtual qint64 getScalar();
 private:
-    QList<Die> m_diceValues;
+    QList<Die*> m_diceValues;
 };
 
 #endif // DICERESULT_H
