72120576b04270ea7ddf207322d29fd81823d243
obiwankennedy <renaud@rolisteam.org>
2014-01-08 18:17:42 +0100

    Update diceresult.cpp
    die*

--- diceresult.cpp
+++ diceresult.cpp
@@ -4,15 +4,15 @@ DiceResult::DiceResult()
 {
 
 }
-void DiceResult::insertResult(Die die)
+void DiceResult::insertResult(Die* die)
 {
     m_diceValues.append(die);
 }
-QList<Die>& DiceResult::getResultList()
+QList<Die*>& DiceResult::getResultList()
 {
     return m_diceValues;
 }
-void DiceResult::setResultList(QList<Die> list)
+void DiceResult::setResultList(QList<Die*> list)
 {
     m_diceValues.clear();
     m_diceValues << list;
@@ -30,7 +30,7 @@ qint64 DiceResult::getScalar()
 
     if(m_diceValues.size()==1)
     {
-        return m_diceValues[0].getValue();
+        return m_diceValues[0]->getValue();
     }
     return 0;
 }
