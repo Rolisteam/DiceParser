be8415d8976fee460710613bcb3403a7f2028aa9
obiwankennedy <renaud@rolisteam.org>
2014-01-10 10:42:31 +0100

    Create explosedicenode.cpp
    adding explosing dice

---
+++ node/explosedicenode.cpp
@@ -0,0 +1,37 @@
+#include "explosedicenode.h"
+
+ExploseDiceNode::ExploseDiceNode()
+    : m_diceResult(new DiceResult())
+{
+    m_result = m_diceResult;
+}
+void ExploseDiceNode::run(ExecutionNode* previous)
+{
+    if((NULL!=previous)&&(NULL!=previous->getResult()))
+    {
+        DiceResult* previous_result = static_cast<DiceResult*>(previous->getResult());
+        if(NULL!=previous_result)
+        {
+            QList<Die*> list = previous_result->getResultList();
+
+
+            foreach(Die* die, list)
+            {
+                while(m_validator->isValid(die))
+                {
+                    die->roll(true);
+                }
+            }
+            m_diceResult->setResultList(list);
+
+            if(NULL!=m_nextNode)
+            {
+                m_nextNode->run(this);
+            }
+        }
+    }
+}
+void ExploseDiceNode::setValidator(Validator* val)
+{
+      m_validator = val;
+}
