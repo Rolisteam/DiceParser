ff2a28dca68119790329b56682aadab9ab825a93
obiwankennedy <renaud@rolisteam.org>
2014-01-14 18:36:51 +0100

    Update rerolldicenode.cpp
    adding previous result API

--- node/rerolldicenode.cpp
+++ node/rerolldicenode.cpp
@@ -11,6 +11,7 @@ void RerollDiceNode::run(ExecutionNode* previous)
     if((NULL!=previous)&&(NULL!=previous->getResult()))
     {
         DiceResult* previous_result = static_cast<DiceResult*>(previous->getResult());
+        m_result->setPrevious(previous_result);
         if(NULL!=previous_result)
         {
             QList<Die*> list = previous_result->getResultList();
