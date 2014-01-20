d38950f415abf80bc3006245f9cb3a629107ca5e
obiwankennedy <renaud@rolisteam.org>
2014-01-20 10:22:05 +0100

    Create parsingtoolbox.h
    parsing tool library

---
+++ parsingtoolbox.h
@@ -0,0 +1,31 @@
+#ifndef PARSINGTOOLBOX_H
+#define PARSINGTOOLBOX_H
+
+#include <QMap>
+
+#include "node/executionnode.h"
+#include "booleancondition.h"
+#include "range.h"
+
+class ParsingToolBox
+{
+public:
+    ParsingToolBox();
+    ExecutionNode* addSort(ExecutionNode* e,bool b);
+    bool readLogicOperator(QString& str,BooleanCondition::LogicOperator& op);
+    Validator* readValidator(QString& str);
+
+
+    /**
+     * @brief readNumber read number in the given str and remove from the string the read character.
+     * @param str the command line
+     * @param myNumber reference to the found number
+     * @return true, succeed to read number, false otherwise.
+     */
+    bool readNumber(QString&  str, int& myNumber);
+
+private:
+        QMap<QString,BooleanCondition::LogicOperator>* m_logicOp;
+};
+
+#endif // PARSINGTOOLBOX_H
