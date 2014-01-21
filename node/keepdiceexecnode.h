3b05641aec765f7f998dc11b6316ec8441b790d6
obiwankennedy <renaud@rolisteam.org>
2014-01-21 16:26:41 +0100

    Update keepdiceexecnode.h
    toString

--- node/keepdiceexecnode.h
+++ node/keepdiceexecnode.h
@@ -11,6 +11,7 @@ public:
 
     virtual void run(ExecutionNode *previous);
     virtual void setDiceKeepNumber(quint64 );
+    virtual QString toString()const;
 private:
     quint64 m_numberOfDice;
     DiceResult* m_diceResult;
