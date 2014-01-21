47095d18478f6f7902a0d6da155843745649fd0c
obiwankennedy <renaud@rolisteam.org>
2014-01-21 16:34:08 +0100

    Update startingnode.cpp
    toString

--- node/startingnode.cpp
+++ node/startingnode.cpp
@@ -7,8 +7,13 @@ StartingNode::StartingNode()
 }
 void StartingNode::run(ExecutionNode*)
 {
+    qDebug() << "starting node";
     if(NULL!=m_nextNode)
     {
         m_nextNode->run(this);
     }
+}
+QString StartingNode::toString() const
+{
+    return "Starting node";
 }
