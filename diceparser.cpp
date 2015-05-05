3c93a8a938d313cbb2e5b880754ac726527a3e55
Renaud G <renaud@rolisteam.org>
2015-05-05 10:35:03 +0200

    removal of translatable string for commands

--- diceparser.cpp
+++ diceparser.cpp
@@ -72,8 +72,8 @@ DiceParser::DiceParser()
 
 
 	m_commandList = new QStringList();
-    m_commandList->append(QObject::tr("help"));
-    m_commandList->append(QObject::tr("la"));
+    m_commandList->append("help");
+    m_commandList->append("la");
 
 }
 DiceParser::~DiceParser()
