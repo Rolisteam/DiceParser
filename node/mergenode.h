f17d771c68dbb80708880e186d14c7e707c1bf4f
Renaud G <renaud@rolisteam.org>
2015-12-24 11:19:55 +0100

    -Adding mergenode to the project.

---
+++ node/mergenode.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+* Copyright (C) 2014 by Renaud Guezennec                                   *
+* http://renaudguezennec.homelinux.org/accueil,3.html                      *
+*                                                                          *
+*  This file is part of DiceParser                                         *
+*                                                                          *
+* DiceParser is free software; you can redistribute it and/or modify       *
+* it under the terms of the GNU General Public License as published by     *
+* the Free Software Foundation; either version 2 of the License, or        *
+* (at your option) any later version.                                      *
+*                                                                          *
+* This program is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+* GNU General Public License for more details.                             *
+*                                                                          *
+* You should have received a copy of the GNU General Public License        *
+* along with this program; if not, write to the                            *
+* Free Software Foundation, Inc.,                                          *
+* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.                 *
+***************************************************************************/
+#ifndef MERGENODE_H
+#define MERGENODE_H
+
+#include "node/executionnode.h"
+#include "result/diceresult.h"
+
+/**
+ * @brief The MergeNode class is an ExecutionNode. It is dedicated to merge result of several commands.
+ */
+class MergeNode : public ExecutionNode
+{
+public:
+    MergeNode();
+    void run(ExecutionNode* previous);
+    virtual QString toString(bool withLabel)const;
+    virtual qint64 getPriority() const;
+private:
+    qint64 m_number;
+    DiceResult* m_diceResult;
+};
+
+#endif // NUMBERNODE_H
