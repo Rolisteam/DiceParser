1224ae137b7e6cd4bfdb9e259ab6cbba11ba1b55
Renaud G <renaud@rolisteam.org>
2017-10-21 18:42:44 +0200

    -improve the test script to get the under dev dice cli version.

--- cli/test_dice.sh
+++ cli/test_dice.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+
+DICE="/home/renaud/application/mine/build-DiceParser-Qt5_7-debug-dice/cli/bin/dice"
+
 export LD_LIBRARY_PATH="/home/renaud/application/other/Qt/5.7/gcc_64/lib:$LD_LIBRARY_PATH"
 for line in `cat cmds.txt`
 do
   #echo $line;
-	dice $line
+	$DICE $line
 done
