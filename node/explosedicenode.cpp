dd5ec0eb06c439781a7f9f06211527f1c51f3165
Renaud G <renaud@rolisteam.org>
2018-03-12 13:40:59 +0100

    fix explosing node

--- node/explosedicenode.cpp
+++ node/explosedicenode.cpp
@@ -10,11 +10,11 @@ void ExploseDiceNode::run(ExecutionNode* previous)
 	m_previousNode = previous;
     if((nullptr!=previous)&&(nullptr!=previous->getResult()))
     {
-        DiceResult* previous_result = static_cast<DiceResult*>(previous->getResult());
+        DiceResult* previous_result = dynamic_cast<DiceResult*>(previous->getResult());
         m_result->setPrevious(previous_result);
         if(nullptr!=previous_result)
         {
-            for(Die* die : previous_result->getResultList())
+            for(Die* die: previous_result->getResultList())
             {
                 Die* tmpdie = new Die();
                 *tmpdie=*die;
@@ -38,6 +38,10 @@ void ExploseDiceNode::run(ExecutionNode* previous)
             {
                 m_nextNode->run(this);
             }
+        }
+        else
+        {
+            qDebug() << "test!!";
         }
     }
 }
@@ -66,12 +70,10 @@ QString ExploseDiceNode::toString(bool withlabel) const
 qint64 ExploseDiceNode::getPriority() const
 {
     qint64 priority=0;
-    if(nullptr!=m_nextNode)
+    if(nullptr!=m_previousNode)
     {
-        priority = m_nextNode->getPriority();
+        priority = m_previousNode->getPriority();
     }
-
-
     return priority;
 }
 
