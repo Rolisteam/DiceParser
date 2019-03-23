d0f326f7289add5d84c662c6c813f1c3c88fca4f
Renaud G <renaud@rolisteam.org>
2019-03-23 01:15:02 +0100

    Add color management for every dice.

--- cli/displaytoolbox.h
+++ cli/displaytoolbox.h
@@ -9,16 +9,22 @@
 class DisplayToolBox
 {
 public:
+    enum class Output
+    {
+        Svg,
+        Terminal,
+        Json,
+        Image
+    };
     DisplayToolBox();
 #ifdef PAINTER_OP
-    static QString makeImage(QString scalarText, QString resultStr, QJsonArray array, bool withColor, QString cmd,
-        QString comment, bool allSameFaceCount, bool allSameColor);
+    static QString makeImage(QByteArray svgCode);
 #endif
     static QString colorToTermCode(QString str);
     static QString diceToText(QJsonArray array, bool withColor, bool allSameFaceCount, bool allSameColor);
     static QJsonArray diceToJson(QList<ExportedDiceResult>& diceList, bool& allSameFaceCount, bool& allSameColor);
     static QString diceToSvg(QJsonArray array, bool withColor, bool allSameColor, bool allSameFaceCount);
-    static QString diceResultToString(QJsonObject val);
+    static QString diceResultToString(QJsonObject val, Output type, bool hasColor);
 };
 
 #endif // GENERATEIMAGE_H
