55191413186381051a1d83b2d41b65dd5d57dfa1
Renaud G <renaud@rolisteam.org>
2019-07-25 22:33:34 +0200

    Fuzzer script

--- tests/fuzzer/run_test.sh
+++ tests/fuzzer/run_test.sh
@@ -3,12 +3,18 @@
 rm -rf build
 mkdir build
 cd build
-CC=afl-gcc CXX=afl-g++ cmake ../ && make
+CC=afl-gcc CXX=afl-g++ cmake ../ -DSTATIC_BUILD=ON && make
 #export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 
 
 #./fuzzTestCase ../testcase_dir/gooddata.txt
-afl-fuzz -m 2G -i ../testcase_dir -o ../findings_dir ./fuzzTestCase @@
+# afl-fuzz -m 2G -i ../testcase_dir -o ../findings_dir ./fuzzTestCase @@
+afl-fuzz -m 2G -i ../testcase_dir -o ../sync_dir -M fuzzer01 ./fuzzTestCase @@
+afl-fuzz -m 2G -i ../testcase_dir -o ../sync_dir -S fuzzer02 ./fuzzTestCase @@
+afl-fuzz -m 2G -i ../testcase_dir -o ../sync_dir -S fuzzer03 ./fuzzTestCase @@
+afl-fuzz -m 2G -i ../testcase_dir -o ../sync_dir -S fuzzer04 ./fuzzTestCase @@
+afl-fuzz -m 2G -i ../testcase_dir -o ../sync_dir -S fuzzer05 ./fuzzTestCase @@
+afl-fuzz -m 2G -i ../testcase_dir -o ../sync_dir -S fuzzer06 ./fuzzTestCase @@
 
 
 
