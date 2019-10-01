5e28288120abb5a4c0ee3f303dd1b98515807df9
Renaud G <renaud@rolisteam.org>
2019-10-01 23:26:21 +0200

    Fix endless loop t operator.

--- node/allsamenode.cpp
+++ node/allsamenode.cpp
@@ -1,6 +1,5 @@
 #include "allsamenode.h"
 
-
 AllSameNode::AllSameNode() : m_diceResult(new DiceResult())
 {
     m_result= m_diceResult;
@@ -12,47 +11,52 @@ void AllSameNode::run(ExecutionNode* previous)
     if(nullptr != previous)
     {
         DiceResult* previous_result= dynamic_cast<DiceResult*>(previous->getResult());
-		if(nullptr != previous_result)
+        if(nullptr != previous_result)
         {
-        	m_result->setPrevious(previous_result);
-			bool allSame=true;
-			int i=0;
-			quint64 previousValue;
-			for(auto& die : previous_result->getResultList())
-            {   
-				if(i == 0)
-					previousValue=die->getValue();
+            m_result->setPrevious(previous_result);
+            bool allSame= true;
+            int i= 0;
+            qint64 previousValue= 0;
+            if(previous_result->getResultList().size() < 2)
+            {
+                m_errors.insert(Dice::ERROR_CODE::ENDLESS_LOOP_ERROR,
+                                QStringLiteral("T operator must operate on more than 1 die"));
+                return;
+            }
+            for(auto& die : previous_result->getResultList())
+            {
+                if(i == 0)
+                    previousValue= die->getValue();
                 Die* tmpdie= new Die(*die);
                 m_diceResult->insertResult(tmpdie);
                 die->displayed();
-				if(previousValue != die->getValue())
-					allSame=false;
-				++i;
+                if(previousValue != die->getValue())
+                    allSame= false;
+                ++i;
             }
 
-			while(allSame)
-			{
-				QList<Die*> list= m_diceResult->getResultList();
-				qint64 pValue;
-				int i =0;
-				for(auto& die: list)
-				{
-					die->roll(true);
-					if(i==0)
-						pValue=die->getValue();
-					if(pValue != die->getValue())
-						allSame = false;
-					++i;
-				}
-			}
-		}
+            while(allSame)
+            {
+                QList<Die*> list= m_diceResult->getResultList();
+                qint64 pValue= 0;
+                int i= 0;
+                for(auto& die : list)
+                {
+                    die->roll(true);
+                    if(i == 0)
+                        pValue= die->getValue();
+                    if(pValue != die->getValue())
+                        allSame= false;
+                    ++i;
+                }
+            }
+        }
     }
     if(nullptr != m_nextNode)
     {
         m_nextNode->run(this);
     }
 }
-
 
 QString AllSameNode::toString(bool withLabel) const
 {
@@ -68,12 +72,12 @@ QString AllSameNode::toString(bool withLabel) const
 
 qint64 AllSameNode::getPriority() const
 {
-     qint64 priority= 0;
-     if(nullptr != m_nextNode)
-     {
+    qint64 priority= 0;
+    if(nullptr != m_nextNode)
+    {
         priority= m_nextNode->getPriority();
-     }
-     return priority;
+    }
+    return priority;
 }
 
 ExecutionNode* AllSameNode::getCopy() const
